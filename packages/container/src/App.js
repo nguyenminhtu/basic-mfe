b2c29980be1517cf6e0ad7abc00adc2a069569ca
Tu Nguyen <tuunguyen2795@gmail.com>
2022-05-26 18:34:06 +0700

    change deploy to DO

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,5 +3,9 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default function App() {
-  return <MarketingApp />;
+  return (
+    <>
+      <MarketingApp />
+    </>
+  );
 }
