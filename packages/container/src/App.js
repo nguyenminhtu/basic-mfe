f01c58a4ef965cd4b17b17c5a7ed4b03a6d80c85
Tu Nguyen <tuunguyen2795@gmail.com>
2022-06-03 15:32:28 +0700

    add app header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default function App() {
-  return <MarketingApp />;
+  return (
+    <Router>
+      <Header />
+      <MarketingApp />
+    </Router>
+  );
 }
