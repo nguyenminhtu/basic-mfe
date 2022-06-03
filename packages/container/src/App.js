076c4bfa8952597d45cad71587914dfaa15d11e2
Tu Nguyen <tuunguyen2795@gmail.com>
2022-06-03 16:12:48 +0700

    fix css collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'main',
+});
 
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <MarketingApp />
-    </Router>
+    <StylesProvider generateClassName={generateClassName}>
+      <Router>
+        <Header />
+        <MarketingApp />
+      </Router>
+    </StylesProvider>
   );
 }
